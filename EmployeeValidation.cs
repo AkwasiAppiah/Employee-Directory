ba313fdf2bcec1838bbabd52d7fb3bd32424a148
Akwasi Appiah <Akwasi.Appiah@capitalontap.com>
2021-11-16 11:29:01 +0000

    Added validation error messsages

--- EmployeeValidation.cs
+++ EmployeeValidation.cs
@@ -7,37 +7,35 @@ namespace Employee_Directory
         public EmployeeValidation()
         {
             // First Name
-            RuleFor(employee => employee.FirstName).Matches(@"^(?!\s*$).+");
+            //Regex for not null
+            RuleFor(employee => employee.FirstName).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank");
 
             //Last Name
-            RuleFor(employee => employee.LastName).Matches(@"^(?!\s*$).+");
+            RuleFor(employee => employee.LastName).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); ;
             
             // Department 
-            RuleFor(employee => employee.Department).Matches(@"^(?!\s*$).+");
+            RuleFor(employee => employee.Department).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); ;
 
             // Gender
-            RuleFor(employee => employee.Gender).Matches(@"^(?!\s*$).+");
+            RuleFor(employee => employee.Gender).Matches(@"^(?:m|M|male|Male|f|F|female|Female)$");
 
             // DOB
-            RuleFor(employee => employee.DoB)
-                .Matches(@"^(?:0[1-9]|[12]\d|3[01])([\/.-])(?:0[1-9]|1[012])\1(?:19|20)\d\d$");
+            RuleFor(employee => employee.DoB).Matches(@"^(?:0[1-9]|[12]\d|3[01])([\/.-])(?:0[1-9]|1[012])\1(?:19|20)\d\d$");
 
             // Address 1
-            RuleFor(employee => employee.Address1).Matches(@"^(?!\s*$).+");
+            RuleFor(employee => employee.Address1).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); ;
             // Address 2
-            RuleFor(employee => employee.Address2).Matches(@"^(?!\s*$).+");
+            RuleFor(employee => employee.Address2).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); ;
             // ... 
-            RuleFor(employee => employee.Address3).Matches(@"^(?!\s*$).+");
+            RuleFor(employee => employee.Address3).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); ;
 
-            RuleFor(employee => employee.Address4).Matches(@"^(?!\s*$).+");
+            RuleFor(employee => employee.Address4).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); ;
 
             //Postcode
-            RuleFor(employee => employee.Postcode)
-                .Matches(
-                    @"^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$");
+            RuleFor(employee => employee.Postcode).Matches(@"^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$").WithMessage("Please enter a valid Postcode");
 
             //Mobile Number
-            RuleFor(employee => employee.MoblieNumber).Matches(@"^(07\d{8,12}|447\d{7,11})$");
+            RuleFor(employee => employee.MoblieNumber).Matches(@"^(07\d{8,12}|447\d{7,11})$").WithMessage("Please enter a valid mobile number");
         }
     }
 }
