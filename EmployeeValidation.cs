77c7e7487dd63c0d28eedc8aff002d9e78f23ef2
Akwasi Appiah <Akwasi.Appiah@capitalontap.com>
2021-11-16 15:44:23 +0000

    final tidy up

--- EmployeeValidation.cs
+++ EmployeeValidation.cs
@@ -11,10 +11,10 @@ namespace Employee_Directory
             RuleFor(employee => employee.FirstName).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank");
 
             //Last Name
-            RuleFor(employee => employee.LastName).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); ;
+            RuleFor(employee => employee.LastName).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); 
             
             // Department 
-            RuleFor(employee => employee.Department).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); ;
+            RuleFor(employee => employee.Department).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); 
 
             // Gender
             RuleFor(employee => employee.Gender).Matches(@"^(?:m|M|male|Male|f|F|female|Female)$");
@@ -23,13 +23,13 @@ namespace Employee_Directory
             RuleFor(employee => employee.DoB).Matches(@"^(?:0[1-9]|[12]\d|3[01])([\/.-])(?:0[1-9]|1[012])\1(?:19|20)\d\d$").WithMessage("dd/mm/yyyy");
 
             // Address 1
-            RuleFor(employee => employee.Address1).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); ;
+            RuleFor(employee => employee.Address1).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); 
             // Address 2
-            RuleFor(employee => employee.Address2).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); ;
+            RuleFor(employee => employee.Address2).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); 
             // ... 
-            RuleFor(employee => employee.Address3).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); ;
+            RuleFor(employee => employee.Address3).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); 
 
-            RuleFor(employee => employee.Address4).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); ;
+            RuleFor(employee => employee.Address4).Matches(@"^(?!\s*$).+").WithMessage("Please do not leave blank"); 
 
             //Postcode
             RuleFor(employee => employee.Postcode).Matches(@"^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$").WithMessage("Please enter a valid Postcode");
